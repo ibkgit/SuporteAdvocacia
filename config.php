2ceedce005262b8d9ea6fb4e18facf60876b1bf9
ibkgit <ibkcorretor@gmail.com>
2023-12-06 15:52:30 -0300

    InstalacaoPHP

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+    $dbHost = 'localhost';
+    $dbUsername = 'ibk'; 
+    $dbPassword = 'sql308197';
+    $dbname = 'advogados';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbname)
+
+    if($conexao->connect_errno)
+    {
+        echo "Erro";
+    }
+    else
+    {
+        "Conexão efetuada com sucesso!!!"
+    }
+?>
